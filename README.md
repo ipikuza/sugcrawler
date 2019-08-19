86e2582af341cc514c060e850592e50edfc84e54
Irina Pikuza <irina.i.pikuza@gmail.com>
2019-08-19 19:41:51 +0300

    Add new file for new project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# sugcrawler" 
+"# sugcrawler" 
---
+++ init_app.py
@@ -0,0 +1,24 @@
+# init_app.py
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sug_config import DB_NAME, RANDOM_STRING, APP_NAME
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(APP_NAME)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(DB_NAME)
+    app.config['SECRET_KEY'] = RANDOM_STRING
+    db.init_app(app)
+
+    with app.app_context():
+        import routes
+
+        # creates only tables that do not exist
+        db.create_all()
+        db.session.commit()
+
+        return app
